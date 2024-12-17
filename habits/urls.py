b95c96c2d860dacfb334ab6b9ce6a331936201f8
mmrxsky <mmrxsky@gmail.com>
2024-12-17 14:49:59 +0300

    fix изменил на класс Generic и обновил шаблон URL для публичных привычек

--- habits/urls.py
+++ habits/urls.py
@@ -1,12 +1,15 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from habits.apps import HabitsConfig
-from habits.views import HabitViewSet, PublicHabitViewSet
+from habits.views import HabitViewSet, PublicHabitListView, PublicHabitDetailView
 
 app_name = HabitsConfig.name
 
 router = SimpleRouter()
 router.register(r"", HabitViewSet)
-router.register(r"public", PublicHabitViewSet, basename="public-habits")
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path('public/', PublicHabitListView.as_view(), name='public-habits-list'),
+    path('public/<int:pk>/', PublicHabitDetailView.as_view(), name='public-habits-detail'),
+] + router.urls
